48b1b39c45dc694b5729584d2b21b31788711dd3
Abhishek <abhi.ghosh27feb@gmail.com>
2022-09-25 16:24:16 +0530

    add

--- src/ParentLifeCycle.js
+++ src/ParentLifeCycle.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react'
 import ChildLifeCycle from './ChildLifeCycle'
 
 export default class ParentLifeCycle extends Component {
+
+
+  //1
     constructor(props){
         super(props)
+
+        this.state={name:'abhi'}
        console.log("constructor called of parent")
     }
+    //2
   render() {
     console.log("render called of parent")
     return (
-      <div>ParentLifeCycle
+      <div>{this.state.name}
 
       <ChildLifeCycle></ChildLifeCycle>
       </div>
       
     )
   }
+  //3
   componentDidMount(){
 
     console.log("componentDidMount called of parent")
