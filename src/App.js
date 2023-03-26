8e8d996d11027b24c4b9de2ab3760b9c6f9e9769
unknown <abhi.ghosh27feb@gmail.com>
2023-03-26 16:49:13 +0530

    add

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import LifecycleA from './LifecycleA';
 import Posts from './Posts';
 import ParentLifeCycle from './ParentLifeCycle';
 import PostData from './PostData';
+import PostApiCall from './PostApiCall';
+import GetApi from './GetApi';
+import Registerform from './Registerform';
+import Forms from './Forms';
 
 function App() {
   return (
     <div className="App">
      {/* <LifecycleA></LifecycleA> */}
-     <ParentLifeCycle></ParentLifeCycle>
+     {/* <PostApiCall></PostApiCall> */}
      {/* <PostData></PostData> */}
+     {/* <GetApi></GetApi> */}
+     <Registerform></Registerform>
+     {/* <Forms></Forms> */}
     </div>
   );
 }
